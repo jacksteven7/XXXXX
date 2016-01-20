b877a433baa0811e41e2fe300dba3c69c9f0f717
jacksteven7 <jacksteven7@gmail.com>
2016-01-20 01:11:15 -0500

    Fixing index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 
 	acts_as_votable
- 	has_attached_file :left, styles: { large: "600x400>" ,medium: "300x200>", thumb: "100x60>" }
- 	has_attached_file :right, styles: { large: "600x400>" ,medium: "300x200>", thumb: "100x60>" }
+ 	has_attached_file :left, styles: { large: "400x400>" ,medium: "200x200>", thumb: "60x60>" }
+ 	has_attached_file :right, styles: { large: "400x400>" ,medium: "200x200>", thumb: "60x60>" }
  	validates_attachment_content_type :left, content_type: /\Aimage\/.*\Z/
  	validates_attachment_content_type :right, content_type: /\Aimage\/.*\Z/
 end
